cc60b363a8a8f109b0a0b9b478bc94c25944857e
Douglas Feng <144007759+buwanyuanshen@users.noreply.github.com>
2023-12-26 15:55:07 +0800

    Update SettingAction.tsx

--- src/components/Chat/SettingAction.tsx
+++ src/components/Chat/SettingAction.tsx
@@ -131,7 +131,7 @@ export default function SettingAction() {
               label="OpenAI 模型"
             >
               <Selector
-                class="max-w-150px"
+                class="max-w-200px"
                 value={store.sessionSettings.model}
                 onChange={e => {
                   setStore(
@@ -142,22 +142,42 @@ export default function SettingAction() {
                 }}
                 options={[
                   {
-                    value: "gpt-3.5",
-                    label: "gpt-3.5(auto)"
+                    value: "gpt-3.5-turbo-1106",
+                    label: "gpt-3.5-turbo-1106"
+                  },
+                  {
+                    value: "gpt-3.5-turbo-16k",
+                    label: "gpt-3.5-turbo-16k"
+                  },
+                  {
+                    value: "gpt-3.5-turbo-16k-0613",
+                    label: "gpt-3.5-turbo-16k-0613"
                   },
                   {
                     value: "gpt-4",
-                    label: "gpt-4(auto)"
+                    label: "gpt-4"
                   },
                   {
-                    value: "gpt-4-preview",
-                    label: "gpt-4(preview)"
+                    value: "gpt-4-1106-preview",
+                    label: "gpt-4-1106-preview"
+                  },
+                  {
+                    value: "gpt-4-32k",
+                    label: "gpt-4-32k"
+                  },
+                  {
+                    value: "gpt-4-32k-0314",
+                    label: "gpt-4-32k-0314"
+                  },
+                  {
+                    value: "gpt-4-32k-0613",
+                    label: "gpt-4-32k-0613"
                   }
                 ]}
               />
             </SettingItem>
             <SettingItem icon="i-carbon:data-enrichment" label="思维发散程度">
-              <div class="flex items-center justify-between w-150px">
+              <div class="flex items-center justify-between w-200px">
                 <input
                   type="range"
                   min={0}
