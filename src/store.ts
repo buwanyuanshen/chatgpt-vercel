9c1140222d0634df1d58ba3655615459fa1734ef
Douglas Feng <144007759+buwanyuanshen@users.noreply.github.com>
2023-12-26 16:28:04 +0800

    Update store.ts

--- src/store.ts
+++ src/store.ts
@@ -100,13 +100,13 @@ const modelFee = {
     input: 0.0015,
     output: 0.002
   },
-  "gpt-4": {
-    input: 0.03,
-    output: 0.06
-  },
   "gpt-4-1106-preview": {
     input: 0.01,
     output: 0.03
+  },
+  "gpt-4": {
+    input: 0.03,
+    output: 0.06
   },
   "gpt-4-32k": {
     input: 0.06,
