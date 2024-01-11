e43c7a63cb254591f4e472cbca403d6890912e99
Douglas Feng <144007759+buwanyuanshen@users.noreply.github.com>
2024-01-11 18:51:38 +0800

    Update SettingAction.tsx

--- src/components/Chat/SettingAction.tsx
+++ src/components/Chat/SettingAction.tsx
@@ -142,6 +142,14 @@ export default function SettingAction() {
                 }}
                 options={[
                   {
+                    value: "babbage-002",
+                    label: "babbage-002"
+                  },
+                  {
+                    value: "davinci-002",
+                    label: "davinci-002"
+                  },
+                  {
                     value: "gpt-3.5-turbo-0301",
                     label: "gpt-3.5-turbo-0301"
                   },
@@ -150,8 +158,7 @@ export default function SettingAction() {
                     label: "gpt-3.5-turbo-0613"
                   },
                   {
-                    value: 
-"gpt-3.5-turbo",
+                    value: "gpt-3.5-turbo",
                     label: "gpt-3.5-turbo"
                   },
                   {
