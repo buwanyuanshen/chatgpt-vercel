cc559bc9c53fbfb1d5eadbdd448f825418189215
Douglas Feng <144007759+buwanyuanshen@users.noreply.github.com>
2024-03-12 23:07:35 +0800

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -13,7 +13,7 @@ export interface ChatMessage {
 }
 
 export type Role = "system" | "user" | "assistant" | "error"
-export type SimpleModel ="gpt-3.5-turbo-0301" | "gpt-3.5-turbo-0613" | "gpt-3.5-turbo" | "gpt-3.5-turbo-1106" | "gpt-3.5-turbo-0125" | "gpt-3.5-turbo-16k" | "gpt-3.5-turbo-16k-0613" | "gpt-4" | "gpt-4-1106-preview" | "gpt-4-32k" | "gpt-4-32k-0314" | "gpt-4-32k-0613"
+export type SimpleModel ="gpt-3.5-turbo-0301" | "gpt-3.5-turbo-0613" | "gpt-3.5-turbo" | "gpt-3.5-turbo-1106" | "gpt-3.5-turbo-0125" | "gpt-3.5-turbo-16k" | "gpt-3.5-turbo-16k-0613" | "gpt-4" | "gpt-4-0125-preview" | "gpt-4-32k" | "gpt-4-32k-0314" | "gpt-4-32k-0613"
 export type Model =
   | "gpt-3.5-turbo-0301"
   | "gpt-3.5-turbo-0613"
@@ -23,7 +23,7 @@ export type Model =
   | "gpt-3.5-turbo-16k"
   | "gpt-3.5-turbo-16k-0613"
   | "gpt-4"
-  | "gpt-4-1106-preview"
+  | "gpt-4-0125-preview"
   | "gpt-4-32k"
   | "gpt-4-32k-0314"
   | "gpt-4-32k-0613"
